9cb0d249e2adc53e9417b5ee0a4e7d3f484f869e
kevin.lemairecolombel <kevin.lemaire.externe@foncia.fr>
2025-10-22 15:31:52 -0400

    final

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -10,13 +10,14 @@ import {
   getLinks,
   updateLinkName,
   updateLinkDestinations,
+  getLast30DaysClicksByCountry,
+  getLast30DaysClicks,
+  getLast24And48HourClicks,
+  totalLinkClickLastHour,
+  activeLinksLastHour,
 } from "@repo/data-ops/queries/links";
 
 import { TRPCError } from "@trpc/server";
-import {
-  ACTIVE_LINKS_LAST_HOUR,
-  LAST_30_DAYS_BY_COUNTRY,
-} from "./dummy-data";
 
 export const linksTrpcRoutes = t.router({
   linkList: t.procedure
@@ -68,23 +69,19 @@ export const linksTrpcRoutes = t.router({
       // FIXME: no security check here?
       await updateLinkDestinations(input.linkId, input.destinations);
     }),
-  activeLinks: t.procedure.query(async () => {
-    return ACTIVE_LINKS_LAST_HOUR;
-  }),
-  totalLinkClickLastHour: t.procedure.query(async () => {
-    return 13;
-  }),
-  last24HourClicks: t.procedure.query(async () => {
-    return {
-      last24Hours: 56,
-      previous24Hours: 532,
-      percentChange: 12,
-    };
-  }),
-  last30DaysClicks: t.procedure.query(async () => {
-    return 78;
-  }),
-  clicksByCountry: t.procedure.query(async () => {
-    return LAST_30_DAYS_BY_COUNTRY;
-  }),
+    activeLinks: t.procedure.query(async ({ctx}) => {
+      return activeLinksLastHour(ctx.userInfo.userId);
+    }),
+    totalLinkClickLastHour: t.procedure.query(async ({ ctx }) => {
+      return await totalLinkClickLastHour(ctx.userInfo.userId);
+    }),
+    last24HourClicks: t.procedure.query(async ({ ctx }) => {
+      return await getLast24And48HourClicks(ctx.userInfo.userId);
+    }),
+    last30DaysClicks: t.procedure.query(async ({ ctx }) => {
+      return await getLast30DaysClicks(ctx.userInfo.userId);
+    }),
+    clicksByCountry: t.procedure.query(async ({ ctx }) => {
+      return await getLast30DaysClicksByCountry(ctx.userInfo.userId);
+    }),
 });
